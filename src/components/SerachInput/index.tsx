738cd3bf8dba8b4f097f345cb8b1c6ed2eb73fcd
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-08-28 23:33:55 -0300

    fix: filter with correct functionalities

--- src/components/SerachInput/index.tsx
+++ src/components/SerachInput/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import style from "./style.module.css";
 import { FiSearch } from "react-icons/fi";
 import { IoMdClose } from "react-icons/io";
@@ -8,17 +8,38 @@ const SearchInput = () => {
 
     const contextData = useContext(MainContext)
     if(!contextData) return null
-    const { searchValue, setSearchValue } = contextData
+    const { searchValue, setSearchValue, setDisplayCountries, allCountries } = contextData
+
+    // const filterCountry = (e: React.FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault()
+    //     setDisplayCountries(allCountries)
+    //     const newDisplay: Array<any> = allCountries.filter( (country: any) => (country.name.toUpperCase() == searchValue.toUpperCase()))
+    //     if(newDisplay) setDisplayCountries(newDisplay)
+    //     setSearchValue('') 
+    // }
+
+    // const search = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     setSearchValue(e.target.value)
+    // };
+
+    useEffect(() => {
+        const newDisplay: Array<any> = allCountries.filter( (country: any) => country.name.toUpperCase().includes(searchValue.toUpperCase()) )
+        setDisplayCountries(newDisplay)
+    }, [searchValue]);
 
     return(
         <div className={style.search_input_container}>
-            <div className={style.search_icon_container}>
-                <FiSearch className={style.search_icon} size={'1.5em'} />
-            </div>
-            <input type="text" className={style.search_input} value={searchValue} placeholder='Search for a country...' onChange={ e => setSearchValue(e.target.value) }/>
-            <div hidden={searchValue.length > 0 ? false : true}>
-                <IoMdClose size={'1.5em'} onClick={() => setSearchValue('')}/>
-            </div>
+            <form className={style.search_form}>
+            {/* <form onSubmit={filterCountry} className={style.search_form}> */}
+                <div className={style.search_icon_container}>
+                    <FiSearch className={style.search_icon} size={'1.5em'} />
+                </div>
+                <input type="text" className={style.search_input} value={searchValue} placeholder='Search for a country...' onChange={e => setSearchValue(e.target.value)}/>
+                {/* <input type="text" className={style.search_input} value={searchValue} placeholder='Search for a country...' onChange={ e => setSearchValue(e.target.value) }/> */}
+                <div hidden={searchValue.length > 0 ? false : true}>
+                    <IoMdClose size={'1.5em'} onClick={() => setSearchValue('')}/>
+                </div>
+            </form>
         </div>
     )
 }
