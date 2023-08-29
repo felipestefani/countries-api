1ab7062cc0887d6ca629f6b4edca51be3bf2cb12
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-08-28 23:34:22 -0300

    fix: routes

--- src/routes/CountryDetails/index.tsx
+++ src/routes/CountryDetails/index.tsx
@@ -1,12 +1,27 @@
 import style from "./style.module.css";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { GoArrowLeft } from "react-icons/go";
+import { useContext } from "react";
+import { MainContext } from "../../contexts/MainContext";
 
 const CountryDetails = () => {
+
+    const contextData = useContext(MainContext)
+    if(!contextData) return null
+    const {allCountries} = contextData
+    
+    const { country_name } = useParams()
+
+    const displayCountry = allCountries.filter( country => country.name == country_name)
+    console.log(displayCountry);
+    
 
     return(
         <div className={style.country_details_container}>
             <Link to={'/'} className={style.back_home}> <GoArrowLeft className={style.back_icon} size={'1.5em'}/>Voltar</Link>
+            <div className={style.country_container}>
+               <img src={displayCountry[0].flag} alt="country flag" className={style.home_flag}/>
+            </div>
         </div>
     )
 }
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,10 +1,15 @@
 import style from "./style.module.css";
 import Card from "../../components/Card";
-import country_data from "../../data/data.json";
 import Filter from "../../components/Filter";
 import SearchInput from "../../components/SerachInput";
+import { useContext, useEffect } from "react";
+import { MainContext } from "../../contexts/MainContext";
 
 const Home = () => {
+
+    const contextData = useContext(MainContext)
+    if(!contextData) return null
+    const { displayCountries } = contextData
 
     return(
         <div className={style.home_container}>
@@ -15,8 +20,8 @@ const Home = () => {
             
             <ul className={style.country_list}>
             {
-                country_data.map( country => (
-                    <li key={country.cioc} >
+                displayCountries.map( country => (
+                    <li key={country.name} >
                         <Card 
                         name={country.name} 
                         population={country.population} 
