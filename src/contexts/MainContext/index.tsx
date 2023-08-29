bb6b36ab6636bdd86a8762682f900aa0840de89e
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-08-28 23:34:56 -0300

    fix: layout components and main page

--- src/contexts/MainContext/index.tsx
+++ src/contexts/MainContext/index.tsx
@@ -1,10 +1,14 @@
 import { createContext, useState, ReactNode } from "react";
+import countries from "../../data/data.json";
 
 interface MainContextType {
-    isDarkMode: boolean;
+    isDarkMode: boolean
     setIsDarkMode: React.Dispatch<React.SetStateAction<boolean>>
-    searchValue: string,
-    setSearchValue: React.Dispatch<React.SetStateAction<string>>,
+    searchValue: string
+    setSearchValue: React.Dispatch<React.SetStateAction<string>>
+    displayCountries: Array<any>
+    setDisplayCountries: React.Dispatch<React.SetStateAction<any[]>>
+    allCountries: Array<any>
 }
 
 export const MainContext = createContext<MainContextType | undefined>(undefined)
@@ -18,9 +22,16 @@ const MainProvider = ({children}: MainProviderProps) => {
 
     const [isDarkMode, setIsDarkMode] = useState(false)
     const [searchValue, setSearchValue] = useState('')
+    const [displayCountries, setDisplayCountries] = useState<Array<any>>(countries)
+
+    const allCountries = countries
 
     return(
-        <MainContext.Provider value={{ isDarkMode, setIsDarkMode, searchValue, setSearchValue }}>
+        <MainContext.Provider value={{  isDarkMode, setIsDarkMode, 
+                                        searchValue, setSearchValue,
+                                        displayCountries, setDisplayCountries,
+                                        allCountries,
+                                    }}>
             {children}
         </MainContext.Provider>
     )
